5271ad0b0677f0fb934e8ac814f4db01a9419d3c
prakash shrestha <shresthaprakash75@gmail.com>
2024-11-23 10:57:54 +0545

    initializing the money flow project

---
+++ moneyflow/moneyflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyflowApp.swift
+//  moneyflow
+//
+//  Created by prakash on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct moneyflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moneyflowTests/moneyflowTests.swift
@@ -0,0 +1,17 @@
+//
+//  moneyflowTests.swift
+//  moneyflowTests
+//
+//  Created by prakash on 23/11/2024.
+//
+
+import Testing
+@testable import moneyflow
+
+struct moneyflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
